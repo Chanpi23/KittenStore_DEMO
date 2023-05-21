454ea13e9a1a82c227f59d718f23aa9dc3e6e763
chanpi23 <channycodez@gmail.com>
2023-05-21 18:17:14 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { Routes, Route } from
- "react-router-dom"
-import { Container} from "react-bootstrap"
-import {Home} from "./pages/Home"
-import {Store} from "./pages/Store"
-import {About} from "./pages/About"
-
+import {Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import {Navbar} from "./components/Navbar"
 
 
 function App() {
   return (
-  <Container>Hi
-<Routes>
-  <Route path = "/" element = {<Home/ >}></Route>
-  <Route path = "/" element = {<Store/ >}></Route>
-  <Route path = "/" element = {<About/ >}></Route>
-</Routes>
-
+  <>
+  <Navbar />
+  <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/store" element={<Store />}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
   </Container>
+  </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css"
-<<<<<<< HEAD
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-=======
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
->>>>>>> 5c8064b0f7be9bb0ffe4553dbb33a9347dedd7b7
   </React.StrictMode>,
 )
