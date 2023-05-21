d1674e27acbf67266437f5589ec7abbb979e9b40
chanpi23 <channycodez@gmail.com>
2023-05-21 16:14:20 -0400

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,23 @@
+import { Routes, Route } from
+ "react-router-dom"
+import { Container} from "react-bootstrap"
+import {Home} from "./pages/Home"
+import {Store} from "./pages/Store"
+import {About} from "./pages/About"
+
+
+
+function App() {
+  return (
+  <Container>Hi
+<Routes>
+  <Route path = "/" element = {<Home/ >}></Route>
+  <Route path = "/" element = {<Store/ >}></Route>
+  <Route path = "/" element = {<About/ >}></Route>
+</Routes>
+
+  </Container>
+  )
+}
 
 export default App
